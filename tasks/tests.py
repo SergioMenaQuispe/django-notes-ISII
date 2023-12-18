77a3ac612454e6dc2afc1ee1ee5b301c0e1e94f8
Bruno <jchancayauris@unsa.edu.pe>
2023-12-18 07:03:32 -0500

    Adicionar caso de prueba 2 : Probando la vista registro y validando el comportamiento en caso se ingresen passwords distintas

--- tasks/tests.py
+++ tasks/tests.py
@@ -44,6 +44,39 @@ class TestUsuarioExistente(TestCase, LiveServerTestCase):
         self.assertEqual(resultado, 'El usuario ya esta registrado.')
 
 
+class TestPasswordsDiferentes(TestCase, LiveServerTestCase):
+    options = webdriver.ChromeOptions()
+    driver = webdriver.Chrome()
+    title = driver.title
+
+    def setUp(self):
+        # Iniciamos el navegador y configuramos
+        self.options.add_argument('--start-maximized')
+        self.options.add_argument('--disable-extensions')
+        self.driver.get('http://127.0.0.1:8000/signup/')
+
+    def tearDown(self) -> None:
+        return super().tearDown()
+
+    def test_form(self):
+        # Intentamos registrarnos
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(
+            (By.ID, 'id_username'))).send_keys('Bruno')
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(
+            (By.ID, 'id_password1'))).send_keys('123')
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(
+            (By.ID, 'id_password2'))).send_keys('321')
+        WebDriverWait(self.driver, 5).until(EC.element_to_be_clickable(
+            (By.XPATH, '/html/body/div/form/div/button'))).click()
+        
+        # Extraemos el mensaje de error generado
+        error = self.driver.find_element(by=By.ID, value='error')
+        # Guardamos el texto de eso mensaje
+        resultado = error.text
+        # Comprobamos si el contenido del mensaje es el esperado
+        self.assertEqual(resultado, 'Las contraseñas no coinciden.')
+
+
 if __name__ == '__main__':
     # Ejecutar las pruebas
     unittest.main()
