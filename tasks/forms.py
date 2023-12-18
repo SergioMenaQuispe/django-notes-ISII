cc8482d7480e6c58711a912a40983ea76c395043
Bruno <jchancayauris@unsa.edu.pe>
2023-12-17 19:33:04 -0500

    Crear carpeta static y clases para los inputs del registro

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.forms import ModelForm
 from .models import Task
 
@@ -5,3 +6,8 @@ class TaskForm(ModelForm):
     class Meta:
         model = Task
         fields = ['title', 'description', 'important']
+        widgets = {
+            'title': forms.title(attrs={'class': 'form-control'}),
+            'description': forms.description(attrs={'class': 'form-control'}),
+            'important': forms.important(attrs={'class': 'form-control'}),
+        }
